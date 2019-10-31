977bff7414dc23650550d0156d2ee443dbdd7fd6
kilmoretrout <ddray@email.unc.edu>
2019-10-31 15:05:01 -0400

    major revision of training script to reflect all other changes

--- src/models/train.py
+++ src/models/train.py
@@ -43,41 +43,43 @@ import matplotlib.pyplot as plt
 def relu_clipped(x):
     return K.relu(x, max_value=1)
 
-print('Python version ' + sys.version)
-print('argparse version ' + argparse.__version__)
-print('numpy version ' + np.__version__)
-print('psutil version ' + psutil.__version__)
-print('tensorflow version ' + tf.__version__)
-print('sklearn version ' + sklearn.__version__)
-print('keras version ' + keras.__version__ + '\n')
+class TrainValTensorBoard(TensorBoard):
+    def __init__(self, log_dir='./logs', **kwargs):
+        # Make the original `TensorBoard` log to a subdirectory 'training'
+        training_log_dir = os.path.join(log_dir, 'training')
+        super(TrainValTensorBoard, self).__init__(training_log_dir, **kwargs)
 
-def parse_args():
-    # Argument Parser    
-    parser = argparse.ArgumentParser()
-    # my args
-    parser.add_argument("--verbose", action = "store_true", help = "display messages")
-    parser.add_argument("--model", default = "UNET_even_smaller.json")
-    parser.add_argument("--data", default = "training.data.npz")
-    parser.add_argument("--tag", default = "test")
-    parser.add_argument("--train_config", default = "training_configs/adam_default", help = "contains the settings for training, the optimizer, learning rate, etc.")
+        # Log the validation metrics to a separate subdirectory
+        self.val_log_dir = os.path.join(log_dir, 'validation')
 
-    parser.add_argument("--odir", default = "training_output")
+    def set_model(self, model):
+        # Setup writer for validation metrics
+        self.val_writer = tf.summary.FileWriter(self.val_log_dir)
+        super(TrainValTensorBoard, self).set_model(model)
 
-    args = parser.parse_args()
+    def on_epoch_end(self, epoch, logs=None):
+        # Pop the validation logs and handle them separately with
+        # `self.val_writer`. Also rename the keys so that they can
+        # be plotted on the same figure with the training metrics
+        logs = logs or {}
+        val_logs = {k.replace('val_', ''): v for k, v in logs.items() if k.startswith('val_')}
+        for name, value in val_logs.items():
+            summary = tf.Summary()
+            summary_value = summary.value.add()
+            summary_value.simple_value = value.item()
+            summary_value.tag = name
+            self.val_writer.add_summary(summary, epoch)
+        self.val_writer.flush()
 
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-        logging.debug("running in verbose mode")
-    else:
-        logging.basicConfig(level=logging.INFO)
+        # Pass the remaining logs to `TensorBoard.on_epoch_end`
+        logs = {k: v for k, v in logs.items() if not k.startswith('val_')}
+        super(TrainValTensorBoard, self).on_epoch_end(epoch, logs)
 
-    if not os.path.exists(args.odir):
-        os.mkdir(args.odir)
-        logging.debug('root: made output directory {0}'.format(args.odir))
+    def on_train_end(self, logs=None):
+        super(TrainValTensorBoard, self).on_train_end(logs)
+        self.val_writer.close()
 
-    return args
-
-def train_cnn_model(model, configFile, weightFileName, xdata_train, ytarget_train, xdata_test, ytarget_test, gpus = 1):
+def train_cnn_model(model, configFile, weightFileName, training_generator, validation_generator, gpus, tf_logdir):
     """
     Compile the model with the adam optimizer and the mean_squared_error loss function.
     Train the model for a given number of epochs in a given batch size until a specified stopping criterion is reached.
@@ -120,23 +122,77 @@ def train_cnn_model(model, configFile, weightFileName, xdata_train, ytarget_trai
                   optimizer='adam',
                   metrics=['accuracy'])
 
+    # Tensor-board callback
+    tbCallBack = TrainValTensorBoard(log_dir = tf_logdir, histogram_freq = 0, write_graph = True, write_images = True, write_grads = False, update_freq = 'epoch')
+
     rlrop = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor = LROnPlateau_factor, patience = LROnPlateau_patience, verbose=1, mode='auto', min_delta=0.0001, cooldown=0, min_lr=0)
 
     earlystop = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience = EarlyStoppingPatience, verbose=0, mode='auto')
     checkpoint = keras.callbacks.ModelCheckpoint(weightFileName, monitor='val_loss', verbose=1, save_best_only=True,
                                                  mode='min')
-    callbacks = [checkpoint, rlrop]
+    callbacks = [earlystop, rlrop, checkpoint]
 
-    history = model.fit(xdata_train, ytarget_train, batch_size=64,
-          epochs=25, verbose=1,
-          validation_data=(xdata_test, ytarget_test), callbacks = callbacks)
+    history = model.fit_generator(generator = training_generator, validation_data = validation_generator,
+                        epochs = n_epochs,
+                        steps_per_epoch = n_steps_per_epoch,
+                        verbose = 1,
+                        use_multiprocessing = False, workers = 1,
+                        callbacks = callbacks)
 
     return history
-    
 
+print('Python version ' + sys.version)
+print('argparse version ' + argparse.__version__)
+print('numpy version ' + np.__version__)
+print('psutil version ' + psutil.__version__)
+print('tensorflow version ' + tf.__version__)
+print('sklearn version ' + sklearn.__version__)
+print('keras version ' + keras.__version__ + '\n')
+
+def parse_args():
+    # Argument Parser    
+    parser = argparse.ArgumentParser()
+    # my args
+    parser.add_argument("--verbose", action = "store_true", help = "display messages")
+    parser.add_argument("--model", default = "UNET_even_smaller.json")
+    parser.add_argument("--data", default = "data_v1.0.hdf5")
+    parser.add_argument("--n_gpus", default = "1")
+    parser.add_argument("--gen_size", default = "64")
+
+    parser.add_argument("--val_prop", default = "0.1")
+    parser.add_argument("--test_prop", default = "0.1")
+    
+    parser.add_argument("--tag", default = "test")
+    parser.add_argument("--train_config", default = "training_configs/adam_default", help = "contains the settings for training, the optimizer, learning rate, etc.")
+
+    parser.add_argument("--odir", default = "training_output")
+    parser.add_argument("--tf_logdir", default = "training_output")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("running in verbose mode")
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if not os.path.exists(args.odir):
+        os.mkdir(args.odir)
+        logging.debug('root: made output directory {0}'.format(args.odir))
+
+    return args
+
+    
 def main():
     ##### Parse arguments
     args = parse_args()
+
+    testProp = float(args.test_prop)
+    valProp = float(args.val_prop)
+    gpus = int(args.n_gpus)
+    tf_logdir = args.tf_logdir
+
+    gen_size = int(args.gen_size)
 
     # Get some output file names
     weightFileName = os.path.join(str(args.odir), '{0}.weights'.format(str(args.tag)))
@@ -145,39 +201,51 @@ def main():
     evalFileName = os.path.join(str(args.odir), '{0}.evals'.format(str(args.tag)))
     profileFileName = os.path.join(str(args.odir), '{0}.profile'.format(str(args.tag)))
 
-    ##### Load the data
-    data = np.load(args.data)
-
-    xdata = data['x']
-    ytarget = data['target']
-    s = xdata.shape
-
-    xdata,ytarget = np.reshape(xdata, newshape=(s[0], s[1], s[2], 1)), np.reshape(ytarget, newshape=(s[0], s[1], s[2], 1))
-
-    xdata_train,ytarget_train = xdata[1000:], ytarget[1000:]
-    xdata_test, ytarget_test  = xdata[:1000], ytarget[:1000]
+    ifiles = [h5py.File(args.data, 'r')]
 
     ##### Load the model from the specified JSON file
-    model = model_from_json(open(args.model, 'r').read(), custom_objects={'relu_clipped': relu_clipped})
+    model = model_from_json(open(args.model, 'r').read())
 
-    history = train_cnn_model(model, args.train_config, weightFileName, xdata_train, ytarget_train, xdata_test, ytarget_test)
+    indices = dict()
+    keys = ['train', 'test', 'val']
 
-    model.load_weights(weightFileName)
+    for key in keys:
+        indices[key] = []
 
-    p = model.predict(xdata_test)
+    for ifile in ifiles:
+        _ = get_partition_indices(list(ifile.keys()), testProp, valProp)
 
-    for i in range(20):
-        plt.subplot(3,1,1)
-        plt.imshow(np.reshape(xdata_test[i], (48, 128)), aspect=.5, cmap='bone')
+        for key in _.keys():
+            indices[key].append(_[key])
 
-        plt.subplot(3,1,2)
-        plt.imshow(np.reshape(ytarget_test[i], (48, 128)), aspect=.5, cmap='bone')
+    n_inputs = 0
 
-        plt.subplot(3,1,3)
-        plt.imshow(np.rint(np.reshape(p[i],  (48, 128))), aspect=.5, cmap='bone')
-        plt.savefig('exmaple_{0:03d}.png'.format(i))
+    input_shapes = []
 
-        plt.close()
+    for layer in model.layers:
+        if type(layer) == InputLayer:
+            n_inputs += 1
+            print(layer.input_shape)
+            input_shapes.append(layer.input_shape)
+
+    params = {'ifiles': ifiles,
+        'n_inputs': n_inputs,
+        'gen_size': gen_size,
+        'input_shapes': input_shapes,
+        }
+
+    print(params)
+    print(model.summary())
+
+    # Generators
+    training_generator = DataGenerator(indices['train'], **params)
+    validation_generator = DataGenerator(indices['val'], **params)
+    test_generator = DataGenerator(indices['test'], **params)
+
+    print([u.shape for u in training_generator[0]])
+
+    history = train_cnn_model(model, args.train_config, weightFileName, training_generator, validation_generator, gpus, tf_logdir)
+
 
 if __name__ == '__main__':
     main()
