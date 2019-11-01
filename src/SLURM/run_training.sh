d0675e68fed17123b7bce8ae1614e66e87691387
kilmoretrout <ddray@email.unc.edu>
2019-11-01 15:44:32 -0400

    updated path of the training python script

--- src/SLURM/run_training.sh
+++ src/SLURM/run_training.sh
@@ -16,5 +16,5 @@ BATCH_SIZE=$4
 date
 hostname
 
-echo singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE}
-singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE}
+echo singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE}
+singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE}
