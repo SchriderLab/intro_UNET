8863ae3b6beb732e79d2b203d0d31d555a71a81b
kilmoretrout <ddray@email.unc.edu>
2019-11-01 15:40:35 -0400

    added more options to SLURM shell script

--- src/SLURM/run_training.sh
+++ src/SLURM/run_training.sh
@@ -9,9 +9,12 @@ unset OMP_NUM_THREADS
 module load singularity/3.0.0
 
 MODEL_FILE=$1
+DATA=$2
+TAG=$3
+BATCH_SIZE=$4
 
 date
 hostname
 
-echo singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 train.py --model ${MODEL_FILE}
-singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 train.py --model ${MODEL_FILE}
+echo singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE}
+singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE}
