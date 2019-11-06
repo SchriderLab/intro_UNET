a07aec43a52c63f43e4ebcc6eabc809fa925571e
kilmoretrout <ddray@email.unc.edu>
2019-11-06 16:22:12 -0500

    updated shell script to reflect changes in train.py

--- src/SLURM/run_training.sh
+++ src/SLURM/run_training.sh
@@ -12,9 +12,10 @@ MODEL_FILE=$1
 DATA=$2
 TAG=$3
 BATCH_SIZE=$4
+INDICES=$5
 
 date
 hostname
 
-echo singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE}
-singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE}
+echo singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE} --indices ${INDICES}
+singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE} --indices ${INDICES}
