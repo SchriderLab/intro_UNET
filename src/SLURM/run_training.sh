65c0a1483a75ee602bcd9901aa0d6fbc2f560d3a
kilmoretrout <ddray@email.unc.edu>
2019-11-06 16:33:06 -0500

    added odir specification

--- src/SLURM/run_training.sh
+++ src/SLURM/run_training.sh
@@ -10,12 +10,13 @@ module load singularity/3.0.0
 
 MODEL_FILE=$1
 DATA=$2
-TAG=$3
-BATCH_SIZE=$4
-INDICES=$5
+ODIR=$3
+TAG=$4
+BATCH_SIZE=$5
+INDICES=$6
 
 date
 hostname
 
-echo singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE} --indices ${INDICES}
-singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE} --indices ${INDICES}
+echo singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE} --indices ${INDICES} --odir {ODIR}
+singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE} --indices ${INDICES} --odir ${ODIR}
