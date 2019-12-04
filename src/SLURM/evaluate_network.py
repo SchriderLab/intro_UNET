e55c4e34716ab7bd79876d00da692e64c8f5d707
kilmoretrout <ddray@email.unc.edu>
2019-12-04 13:40:58 -0500

    added support for bridges

--- src/SLURM/evaluate_network.py
+++ src/SLURM/evaluate_network.py
@@ -2,6 +2,8 @@ import os
 import logging, argparse
 import itertools
 
+import platform
+
 batch_sizes = [128]
 losses = ['mixed']
 
@@ -38,8 +40,10 @@ def main():
     args = parse_args()
 
     # model, data, output directory, tag, batch size, indices
-    if args.cluster == 'longleaf':
+    if platform.platform() == 'Linux-3.10.0-1062.el7.x86_64-x86_64-with-redhat-7.7-Maipo':
         cmd = 'sbatch --partition=volta-gpu  --gres=gpu:{7} --time=2-00:00:00 --qos=gpu_access src/SLURM/run_training.sh {0} {1} {2} {3} {4} {5} {6} {7}'
+    elif platform.platform() == 'Linux-3.10.0-957.27.2.el7.x86_64-x86_64-with-centos-7.6.1810-Core':
+        cmd = 'sbatch --partition=GPU --gres=gpu:p100:2 --time=2-00:00:00 src/SLURM/run_training.sh {0} {1} {2} {3} {4} {5} {6} {7}'
 
     todo = list(itertools.product(batch_sizes, losses))
 
