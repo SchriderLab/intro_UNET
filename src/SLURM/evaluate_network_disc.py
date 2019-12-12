7dc07c363dd87ad7b2eea5754ce3caf0a46de618
kilmoretrout <ddray@email.unc.edu>
2019-12-12 18:40:44 -0500

    added stuff for sweep over discriminator problem

---
+++ src/SLURM/evaluate_network_disc.py
@@ -0,0 +1,63 @@
+import os
+import logging, argparse
+import itertools
+
+import platform
+
+batch_sizes = [32, 64, 96, 128, 256]
+losses = ['categorical_crossentropy']
+
+config_dir = 'training_configs'
+
+def parse_args():
+    # Argument Parser
+    parser = argparse.ArgumentParser()
+    # my args
+    parser.add_argument("--verbose", action="store_true", help="display messages")
+    parser.add_argument("--model", default = "None")
+    parser.add_argument("--data", default = "None")
+    parser.add_argument("--indices", default = "None")
+    parser.add_argument("--odir", default = "None")
+    parser.add_argument("--tag", default = "None")
+
+    parser.add_argument("--batch_size", default = "144")
+
+    parser.add_argument("--cluster", default = "longleaf")
+
+    parser.add_argument("--n_gpus", default = "2")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("running in verbose mode")
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    return args
+
+def main():
+    args = parse_args()
+
+    # model, data, output directory, tag, batch size, indices
+    if platform.platform() == 'Linux-3.10.0-1062.el7.x86_64-x86_64-with-redhat-7.7-Maipo':
+        cmd = 'sbatch --partition=volta-gpu  --gres=gpu:{7} --time=2-00:00:00 --qos=gpu_access src/SLURM/run_training_disc.sh {0} {1} {2} {3} {4} {5} {6} {7}'
+    elif platform.platform() == 'Linux-3.10.0-957.27.2.el7.x86_64-x86_64-with-centos-7.6.1810-Core':
+        cmd = 'sbatch --partition=GPU --gres=gpu:p100:2 --time=2-00:00:00 src/SLURM/run_training_disc.sh {0} {1} {2} {3} {4} {5} {6} {7}'
+
+    todo = list(itertools.product(batch_sizes, losses))
+
+    for bs, loss in todo:
+
+        tag = args.tag + '_{0}_{1}'.format(bs, loss)
+
+        cmd_ = cmd.format(args.model, args.data, args.odir, tag, bs, args.indices, os.path.join(config_dir, loss), args.n_gpus)
+        print(cmd_)
+
+        os.system(cmd_)
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ src/SLURM/run_training_disc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=cnn_binary_class_gpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=35G
+
+unset OMP_NUM_THREADS
+
+module load singularity/3.0.0
+
+MODEL_FILE=$1
+DATA=$2
+ODIR=$3
+TAG=$4
+BATCH_SIZE=$5
+INDICES=$6
+CONFIG=$7
+N_GPUS=$8
+
+mkdir -p ${ODIR}
+
+date
+hostname
+
+echo singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train_discriminator.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE} --indices ${INDICES} --odir ${ODIR} --train_config ${CONFIG} --n_gpus ${N_GPUS}
+singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/train_discriminator.py --model ${MODEL_FILE} --data ${DATA} --tag ${TAG} --gen_size ${BATCH_SIZE} --indices ${INDICES} --odir ${ODIR} --train_config ${CONFIG} --n_gpus ${N_GPUS}
