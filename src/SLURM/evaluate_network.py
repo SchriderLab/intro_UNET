6535d8da6eeeb7462d70281c0e2b78cbe66e94df
kilmoretrout <ddray@email.unc.edu>
2019-12-19 17:14:10 -0500

    added other architectures for variable image size U-Net training

--- src/SLURM/evaluate_network.py
+++ src/SLURM/evaluate_network.py
@@ -4,8 +4,8 @@ import itertools
 
 import platform
 
-batch_sizes = [24, 48, 72, 144, 288]
-losses = ['binary_crossentropy', 'dice_coef', 'mixed']
+batch_sizes = [48]
+losses = ['mixed']
 
 config_dir = 'training_configs'
 
@@ -14,7 +14,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     # my args
     parser.add_argument("--verbose", action="store_true", help="display messages")
-    parser.add_argument("--model", default = "None")
+    parser.add_argument("--idir", default = "None")
     parser.add_argument("--data", default = "None")
     parser.add_argument("--indices", default = "None")
     parser.add_argument("--odir", default = "None")
@@ -44,14 +44,18 @@ def main():
         cmd = 'sbatch --partition=volta-gpu  --gres=gpu:{7} --time=2-00:00:00 --qos=gpu_access src/SLURM/run_training.sh {0} {1} {2} {3} {4} {5} {6} {7}'
     elif platform.platform() == 'Linux-3.10.0-957.27.2.el7.x86_64-x86_64-with-centos-7.6.1810-Core':
         cmd = 'sbatch --partition=GPU --gres=gpu:p100:2 --time=2-00:00:00 src/SLURM/run_training.sh {0} {1} {2} {3} {4} {5} {6} {7}'
+    else:
+        cmd = 'sbatch --partition=volta-gpu  --gres=gpu:{7} --time=2-00:00:00 --qos=gpu_access src/SLURM/run_training.sh {0} {1} {2} {3} {4} {5} {6} {7}'
 
-    todo = list(itertools.product(batch_sizes, losses))
+    models = os.listdir(args.idir)
 
-    for bs, loss in todo:
+    todo = list(itertools.product(batch_sizes, losses, [os.path.join(args.idir, u) for u in models]))
 
-        tag = args.tag + '_{0}_{1}'.format(bs, loss)
+    for bs, loss, model in todo:
 
-        cmd_ = cmd.format(args.model, args.data, args.odir, tag, bs, args.indices, os.path.join(config_dir, loss), args.n_gpus)
+        tag = args.tag + '_{0}_{1}_{2}'.format(bs, loss, model)
+
+        cmd_ = cmd.format(model, args.data, args.odir, tag, bs, args.indices, os.path.join(config_dir, loss), args.n_gpus)
         print(cmd_)
 
         os.system(cmd_)
