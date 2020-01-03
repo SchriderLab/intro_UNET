3812d42e11026d324fa90034aee621e821b32b15
kilmoretrout <ddray@email.unc.edu>
2020-01-03 12:20:36 -0500

    added R script for training the ArchIE model on longleaf

---
+++ src/models/train_ArchIE_64.R
@@ -0,0 +1,9 @@
+library(data.table)
+train <- data.frame(fread("../simulations/training_data.txt")) ## read in the training data
+
+LABEL_COL=73
+
+lr <- train[,1:LABEL_COL] # cut down the extra columns -- 209 is the label (0=not archaic, 1=archaic)
+model <- glm(V73 ~ .,family=binomial(link='logit'),data=lr) # train the model
+save.image("trained_model_ArchIE_64.Rdata") # save the trained model so we don't have to train it again. can load it with load("trained_model.Rdata")
+
