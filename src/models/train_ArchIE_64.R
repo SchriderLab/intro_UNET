f3bc9bc0f553879bc7045dc5b2b8f3a5cecc8182
kilmoretrout <ddray@email.unc.edu>
2020-01-16 18:29:47 -0500

    added automatic model trimming and changed path name

--- src/models/train_ArchIE_64.R
+++ src/models/train_ArchIE_64.R
@@ -5,5 +5,25 @@ LABEL_COL=73
 
 lr <- train[,1:LABEL_COL] # cut down the extra columns -- 209 is the label (0=not archaic, 1=archaic)
 model <- glm(V73 ~ .,family=binomial(link='logit'),data=lr) # train the model
-save(model, "trained_model_ArchIE_64.Rdata") # save the trained model so we don't have to train it again. can load it with load("trained_model.Rdata")
+
+cleanModel2 = function(cm) {
+  cm$y = c()
+  cm$model = c()
+
+  cm$residuals = c()
+  cm$fitted.values = c()
+  cm$effects = c()
+  cm$qr$qr = c()
+  cm$linear.predictors = c()
+  cm$weights = c()
+  cm$prior.weights = c()
+  cm$data = c()
+  cm
+}
+
+model <- cleanModel2(model)
+
+print(model)
+
+save(model, file = "trained_model_ArchIE_64.Rdata") # save the trained model so we don't have to train it again. can load it with load("trained_model.Rdata")
 
