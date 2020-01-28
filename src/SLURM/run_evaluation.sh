e31d777b04bf331d10f39d24b0e4b0a6d5e27376
kilmoretrout <ddray@email.unc.edu>
2020-01-28 16:40:53 -0500

    added slurm script for evaluating models

---
+++ src/SLURM/run_evaluation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=cnn_binary_class_gpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=35G
+
+unset OMP_NUM_THREADS
+
+module load singularity/3.0.0
+
+DATA=$1
+IX=$2
+OFILE=$3
+
+echo singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/get_evaluation_graphs.py --data ${DATA} --ix ${IX} --ofile ${OFILE}
+singularity exec --nv -B /pine -B /proj ${SIMG_PATH} python3 src/models/get_evaluation_graphs.py --data ${DATA} --ix ${IX} --ofile ${OFILE}
