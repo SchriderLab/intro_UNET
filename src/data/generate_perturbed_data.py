77a8283bf322e492431e11a29c780bdcb734a424
kilmoretrout <ddray@email.unc.edu>
2020-02-17 20:27:16 -0500

    added new perturbed data generation

--- src/data/generate_perturbed_data.py
+++ src/data/generate_perturbed_data.py
@@ -5,7 +5,7 @@ ST = [1, 4, 16]
 MT = [0.1, 0.25, 0.75]
 donor_pops = [1, 2, 3]
 
-cmd = 'python3 src/data/generate_data.py --odir /proj/dschridelab/introgression_data/sims_perturbed/{0}_{1}_{2}_128 --st {0} --mt {1} --n_per_pop 64 --donor_pop {2}'
+cmd = 'python3 src/data/generate_data.py --odir /proj/dschridelab/introgression_data/sims_perturbed_v2/{0}_{1}_{2}_128 --st {0} --mt {1} --n_per_pop 64 --donor_pop {2}'
 
 possible = itertools.product(ST, MT, donor_pops)
 
