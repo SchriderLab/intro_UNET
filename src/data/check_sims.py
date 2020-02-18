289f9f867576dfb3b2ade0db160cccf17869f54e
kilmoretrout <ddray@email.unc.edu>
2020-02-18 15:45:07 -0500

    wrote small script to check that all gzipped files will read

---
+++ src/data/check_sims.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+import logging, argparse
+
+from data_functions import *
+
+import h5py
+
+import matplotlib.pyplot as plt
+
+from calc_stats_ms import *
+
+def parse_args():
+    # Argument Parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", action = "store_true", help = "display messages")
+    parser.add_argument("--idir", default = "None")
+
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    args = parse_args()
+
+    ms_files = sorted([os.path.join(args.idir, u) for u in os.listdir(args.idir) if 'ms.gz' in u])
+    log_files = sorted([os.path.join(args.idir, u) for u in os.listdir(args.idir) if 'log.gz' in u])
+    out_files = sorted([os.path.join(args.idir, u) for u in os.listdir(args.idir) if '.out' in u])
+
+    for ix in range(len(ms_files)):
+        try:
+            X_data, P, itarget, iintrog_reg = load_data_ghost(ms, log, 128, int(args.n_individuals))
+        except:
+            print(ms)
+            print(log)
+
+if __name__ == '__main__':
+    main()
+
