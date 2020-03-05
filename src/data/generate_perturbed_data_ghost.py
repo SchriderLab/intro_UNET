a3463c5b64aa57246065d28c61a369aa50dfe363
kilmoretrout <ddray@email.unc.edu>
2020-03-05 16:03:36 -0500

    changed the number of jobs

--- src/data/generate_perturbed_data_ghost.py
+++ src/data/generate_perturbed_data_ghost.py
@@ -5,9 +5,11 @@ ST = [1, 4, 16]
 MT = [0.1, 0.25, 0.75]
 MP = [0.1, 0.2, 0.25]
 
-cmd = 'python3 src/data/generate_data.py --odir /proj/dschridelab/introgression_data/sims_perturbed_ghost/{0}_{1}_{2}_200 --st {0} --mt {1} --mp {2} --n_per_pop 200 --n_jobs 12500'
+cmd = 'python3 src/data/generate_data_ghost.py --odir /proj/dschridelab/introgression_data/sims_perturbed_ghost/{0}_{1}_{2}_200 --st {0} --mt {1} --mp {2} --n_per_pop 200 --n_jobs 1250'
 
-possible = itertools.product(ST, MT, MP)
+possible = list(itertools.product(ST, MT, MP))
+
+print(len(possible))
 
 for i, j, k in possible:
     os.system(cmd.format(i, j, k))
