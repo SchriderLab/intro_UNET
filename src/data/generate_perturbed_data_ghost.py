731aeb0009363a0a1556522cd6878c512af1f784
kilmoretrout <ddray@email.unc.edu>
2020-03-05 16:07:09 -0500

    fixed parameters

--- src/data/generate_perturbed_data_ghost.py
+++ src/data/generate_perturbed_data_ghost.py
@@ -1,9 +1,9 @@
 import os
 import itertools
 
-ST = [1, 4, 16]
-MT = [0.1, 0.25, 0.75]
-MP = [0.1, 0.2, 0.25]
+ST = [0.6, 1.2, 2.4]
+MT = [0.1, 0.2, 0.25]
+MP = [0.005, 0.02, 0.08]
 
 cmd = 'python3 src/data/generate_data_ghost.py --odir /proj/dschridelab/introgression_data/sims_perturbed_ghost/{0}_{1}_{2}_200 --st {0} --mt {1} --mp {2} --n_per_pop 200 --n_jobs 1250'
 
