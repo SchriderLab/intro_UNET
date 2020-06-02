f1ac42e62f6bd44bcdeed3064f4d3cf86af24401
kilmoretrout <ddray@email.unc.edu>
2020-06-02 16:20:29 -0400

    made script potentially take a directory instead of a list of data files, even simpler

--- src/SLURM/run_training.py
+++ src/SLURM/run_training.py
@@ -13,6 +13,8 @@ def parse_args():
     parser.add_argument("--odir", default = "None")
     parser.add_argument("--tag", default = "None")
 
+    parser.add_argument("--idir", default = "None")
+
     parser.add_argument("--indices", default="None")
 
     parser.add_argument("--data", default = "None")
@@ -20,7 +22,7 @@ def parse_args():
     parser.add_argument("--architecture", default="architectures/var_size_two_channel/densenet169.json")
     parser.add_argument("--n_gpus", default="1")
     parser.add_argument("--batch_size", default="32")
-    parser.add_argument("--training_config", default="training_configs/categorical_crossentropy")
+    parser.add_argument("--training_config", default="training_configs/binary_crossentropy")
 
     args = parser.parse_args()
 
@@ -35,6 +37,11 @@ def parse_args():
 
 def main():
     args = parse_args()
+
+    if args.idir == "None":
+        data = args.data
+    else:
+        data = ','.join([os.path.join(args.idir, u) for u in os.listdir(args.idir)])
 
     # works on Longleaf
     cmd = 'sbatch --partition=volta-gpu  --gres=gpu:{7} --time=2-00:00:00 --qos=gpu_access src/SLURM/run_training.sh {0} {1} {2} {3} {4} {5} {6} {7}'
